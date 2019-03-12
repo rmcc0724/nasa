505bb23bc8183437ac1882926e32eb2c55aed6a3
rmcc0724 <rmcc0724@gmail.com>
2019-03-12 23:59:28 +0000

    Api coming along

--- api.js
+++ api.js
@@ -1,11 +1,18 @@
-async function getResults(e) {
+const axios = require('axios');
+
+async function getResults() {
   try {
     const result = await axios('https://api.nasa.gov/neo/rest/v1/feed?start_date=2019-03-18&api_key=DEMO_KEY');
     const data = result.data;
-    console.log(data.near_earth_objects);
+    const items = data.near_earth_objects;
+
+    for (var key in items) {
+        Object.keys(items).map((item) => {console.log("item", items[item])})
+    }
   }
-  catch(error) {
-    console.log("Error" +  error);
+  catch (error) {
+    console.log("Error Happened " + error);
   }
 }
-console.log(getResults());
+
+getResults();
