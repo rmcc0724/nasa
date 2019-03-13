4f211b09ed7c41ccd05aa43df8c3b84357b92d77
rmcc0724 <rmcc0724@gmail.com>
2019-03-13 01:04:14 +0000

    Api configured

--- api.js
+++ api.js
@@ -2,17 +2,27 @@ const axios = require('axios');
 
 async function getResults() {
   try {
-    const result = await axios('https://api.nasa.gov/neo/rest/v1/feed?start_date=2019-03-18&api_key=DEMO_KEY');
+    const result = await axios('https://api.nasa.gov/neo/rest/v1/feed?start_date=2019-03-18&api_key=xtcQn1fI4aTFJGdXDuVKxHMrUOEQIQbN6lYtSf4K');
     const data = result.data;
     const items = data.near_earth_objects;
+    console.log(items);
+    Object.keys(items).map((item) => {
+      
+    items[item].forEach(e => {
+      const html = `<ul>
+<li>Astroid name: ${e.name}</li>
+<li>Date: ${item}</li>
+<li>Diameter: ${e.estimated_diameter.miles.estimated_diameter_max} miles</li>
+<li>Potentially Hazardous: ${e.is_potentially_hazardous_asteroid}</li>
+<li>Missed Earth by ${e.close_approach_data[0].miss_distance.miles} miles</li>
+</ul>`;      
+            console.log(html);
+    })       
 
-    for (var key in items) {
-        Object.keys(items).map((item) => {console.log("item", items[item])})
-    }
+    });
   }
   catch (error) {
     console.log("Error Happened " + error);
   }
 }
-
 getResults();
