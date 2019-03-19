633d5361e9b7f6cc7e2ba72d47f18554343194e3
rmcc0724 <rmcc0724@gmail.com>
2019-03-19 17:13:31 +0000

    Search model base file updated

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,25 +1,23 @@
+//Create an object that uses properties for each of the event listener classes
 export const elements = {
     searchForm: document.querySelector('.search'),
     searchInput: document.querySelector('.search__field'),
     searchRes: document.querySelector('.results'),
     searchResList: document.querySelector('.results__list'),
     searchResPages: document.querySelector('.results__pages'),
-    recipe: document.querySelector('.recipe'),
-    shopping: document.querySelector('.shopping__list'),
-    shoppingDelete: document.querySelector('.shopping__delete'),
+    asteroid: document.querySelector('.asteroid'),
     likesMenu: document.querySelector('.likes__field'),
     likesList: document.querySelector('.likes__list'),
-    deleteBtn: document.querySelector('.delete__btn'),
-    addCart: document.querySelector('.add__cart'),
-    cartQty: document.querySelector('.cart__qty'),
-    cartUnit: document.querySelector('#cart__unit'),
-    cartItem: document.querySelector('.cart__item')
 };
 
+//Create a variable loader and assign a string with the same name to it.
 export const elementStrings = {
     loader: 'loader'
 };
 
+
+//Create a variable renderLoader and use the variable elementStrings.loader as the class name in the div
+//This will render when certain conditions are true 
 export const renderLoader = parent => {
     const loader = `
         <div class="${elementStrings.loader}">
@@ -28,9 +26,13 @@ export const renderLoader = parent => {
             </svg>
         </div>
     `;
+
+    //Insert the loader after the beginning parent html element
     parent.insertAdjacentHTML('afterbegin', loader);
 };
 
+
+//Create a variable clearLoader and use the variable elementStrings.loader as the class to clear it when the items have loaded
 export const clearLoader = () => {
     const loader = document.querySelector(`.${elementStrings.loader}`);
     if (loader) loader.parentElement.removeChild(loader);
