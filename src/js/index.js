b342d6e21dfb2e47201ee4fa73d4dfdc1349bc9c
rmcc0724 <rmcc0724@gmail.com>
2019-03-19 00:54:28 +0000

    Search model part 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,26 @@
-// const axios = require('axios');
-// async function getResults() {
-//     try {
-//         const result = await axios('https://api.nasa.gov/neo/rest/v1/feed?start_date=2019-03-18&api_key=xtcQn1fI4aTFJGdXDuVKxHMrUOEQIQbN6lYtSf4K');
-//         const items = result.data.near_earth_objects;
-//         console.log(items);
-//         //     Object.keys(items).map((item) => {
-//         //       let html = `<h2>Date: ${item}</h2><br/>`;
-
-//         //       items[item].forEach(e => {
-//         //         html += `<ul><li>Astroid name: ${e.name}</li>
-//         // <li>Diameter: ${e.estimated_diameter.miles.estimated_diameter_max} miles</li>
-//         // <li>Potentially Hazardous: ${e.is_potentially_hazardous_asteroid}</li>
-//         // <li>Missed Earth by ${e.close_approach_data[0].miss_distance.miles} miles</li>
-//         // </ul><br/>`;
-//     }
-//     catch (error) {
-//         console.log("Error Happened " + error);
-//     }
-// }
-// getResults();
-
-
 //Import the Search model from the Search.js file
 import Search from './models/Search';
 
-//Create a new Search model with the search query as the argument
-const search = new Search('2019-03-18');
+//Declare a new state variable
+const state = {};
 
+//Here we create the search controller
+const controlSearch = async() => {
 
-console.log(search);
+    //1. Get query from view
+    const query = '2019-03-18';
 
-//Call the getResults() method on the new instantiated Search model
-search.getResults();
+    //2. Create a Search object and add to the state
+    if (query) {
+        state.search = new Search(query);
+    }
+    //3. Prepare UI for the results
+
+    //4. Search for the asteroids
+    await state.search.getResults();
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+    event.preventDefault();
+    controlSearch();
+});
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -8,7 +8,7 @@ export default class Search {
 
     async getResults() {
         try {
-            const res = await axios(`${proxy}https://api.nasa.gov/neo/rest/v1/feed?start_date=2019-03-18&api_key=${key}`);
+            const res = await axios(`${proxy}https://api.nasa.gov/neo/rest/v1/feed?start_date=${this.query}&api_key=${key}`);
             this.result = res.data.near_earth_objects;
             console.log(this.result);
         }
