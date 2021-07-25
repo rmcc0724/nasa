92d77f401aa615b9dc3ca3be33f608ac5b5d52ba
Reid McCulloch <rmcc0724@gmail.com>
2021-07-24 17:03:15 -0700

    nasa v2 setup cont

--- client/src/js/views/asteroidView.js
+++ client/src/js/views/asteroidView.js
@@ -1,101 +1,142 @@
-import { elements } from "./base";
+import View from './View.js';
 
-export const clearAsteroid = () => {
-  elements.introText.style.display = "none";
-  elements.asteroidView.innerHTML = "";
-  clearCloseData();
-};
+// import icons from '../img/icons.svg'; // Parcel 1
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+import { Fraction } from 'fractional';
 
-export const clearCloseData = () => {
-  elements.asteroidCloseList.innerHTML = "";
-  elements.asteroidClosePages.innerHTML = "";
-};
+class asteroidView extends View {
+  _parentElement = document.querySelector('.recipe');
+  _errorMessage = 'We could not find that recipe. Please try another one!';
+  _message = '';
 
-export const renderAsteroid = (asteroid) => {
-
-  const markup = `
-        <figure class="recipe__fig">
-            <img src="img/vesta_0715.jpg" alt="${
-              asteroid.name
-            }" class="recipe__img">
-            <h1 class="recipe__title">
-                <span>${asteroid.name}</span>
-            </h1>
-        </figure>
-        <div class="recipe__details">
-          <div class="recipe__ingredients">            
-          <button class="recipe__love svg recipe_loved svg">
-          <svg class="header__likes">
-              <use href="img/book-mark.svg#book-mark"></use>
-          </svg>
-      </button>
-            <ul class="recipe__ingredient-list">
-            <li>Name: ${asteroid.name}</li>
-            <li>Diameter: ${asteroid.diameter} miles</li>
-            <li>Hazardous: ${asteroid.hazardous}</li>
-            </ul>
-
-          </div>
-          <div class="recipe__ingredients">
-             <h4>Close Approach Data:</h4><br>
-          </div>
-</div>
-          </button>
-        </div>`;
-  elements.asteroidView.insertAdjacentHTML("afterbegin", markup);
-};
-
-export const renderCloseResults = (data, page = 1, resPerPage = 5) => {
-  // elements.asteroidCloseList.innerHTML = "";
-  const start = (page - 1) * resPerPage;
-  const end = page * resPerPage;
-  data.slice(start, end).forEach(renderApproachData);
-  renderButtons(page, data.length, resPerPage);
-};
-
-const renderApproachData = e => {
-  const markup = `<li>Date: ${e.close_approach_date}</li>
-                <li>Miss Distance: ${e.miss_distance.miles} miles</li>
-                <li>Orbiting Body: ${e.orbiting_body}</li>
-                <li>Relative Velocity: ${
-                  e.relative_velocity.miles_per_hour
-                }mph</li><br>`;
-  elements.asteroidCloseList.insertAdjacentHTML("beforeend", markup);
-};
-
-// type: 'prev' or 'next'
-const createButton = (page, type) => `
-    <button class="btn-inline results__btn--${type}" data-goto=${
-  type === "prev" ? page - 1 : page + 1
-}>
-        <span>Page ${type === "prev" ? page - 1 : page + 1}</span>
-        <svg class="search__icon">
-            <use href="img/icons.svg#icon-triangle-${
-              type === "prev" ? "left" : "right"
-            }"></use>
-        </svg>
-    </button>
-`;
-
-const renderButtons = (page, numResults, resPerPage) => {
-  const pages = Math.ceil(numResults / resPerPage);
-  //console.log(`${page} ${numResults} ${resPerPage}`);
-  let button;
-  if (page === 1 && pages > 1) {
-    // Only button to go to next page
-    button = createButton(page, "next");
-  } else if (page < pages) {
-    // Both buttons
-    button = `
-            ${createButton(page, "prev")}
-            ${createButton(page, "next")}
-        `;
-  } else if (page === pages && pages > 1) {
-    // Only button to go to prev page
-    button = createButton(page, "prev");
-  } else if (page === pages && pages === 1) {
-    // Only button to go to prev page
-    button = "";
+  addHandlerRender(handler) {
+    ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
   }
-  elements.asteroidClosePages.insertAdjacentHTML("afterbegin", button);
-};
+
+  addHandlerUpdateServings(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--update-servings');
+      if (!btn) return;
+      const { updateTo } = btn.dataset;
+      if (+updateTo > 0) handler(+updateTo);
+    });
+  }
+
+  addHandlerAddBookmark(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--bookmark');
+      if (!btn) return;
+      handler();
+    });
+  }
+
+  _generateMarkup() {
+    return `
+      <figure class="recipe__fig">
+        <img src="${this._data.image}" alt="${
+      this._data.title
+    }" class="recipe__img" />
+        <h1 class="recipe__title">
+          <span>${this._data.title}</span>
+        </h1>
+      </figure>
+
+      <div class="recipe__details">
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="${icons}#icon-clock"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--minutes">${
+            this._data.cookingTime
+          }</span>
+          <span class="recipe__info-text">minutes</span>
+        </div>
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="${icons}#icon-users"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--people">${
+            this._data.servings
+          }</span>
+          <span class="recipe__info-text">servings</span>
+
+          <div class="recipe__info-buttons">
+            <button class="btn--tiny btn--update-servings" data-update-to="${
+              this._data.servings - 1
+            }">
+              <svg>
+                <use href="${icons}#icon-minus-circle"></use>
+              </svg>
+            </button>
+            <button class="btn--tiny btn--update-servings" data-update-to="${
+              this._data.servings + 1
+            }">
+              <svg>
+                <use href="${icons}#icon-plus-circle"></use>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div class="recipe__user-generated ${this._data.key ? '' : 'hidden'}">
+          <svg>
+            <use href="${icons}#icon-user"></use>
+          </svg>
+        </div>
+        <button class="btn--round btn--bookmark">
+          <svg class="">
+            <use href="${icons}#icon-bookmark${
+      this._data.bookmarked ? '-fill' : ''
+    }"></use>
+          </svg>
+        </button>
+      </div>
+
+      <div class="recipe__ingredients">
+        <h2 class="heading--2">Recipe ingredients</h2>
+        <ul class="recipe__ingredient-list">
+          ${this._data.ingredients.map(this._generateMarkupIngredient).join('')}
+      </div>
+
+      <div class="recipe__directions">
+        <h2 class="heading--2">How to cook it</h2>
+        <p class="recipe__directions-text">
+          This recipe was carefully designed and tested by
+          <span class="recipe__publisher">${
+            this._data.publisher
+          }</span>. Please check out
+          directions at their website.
+        </p>
+        <a
+          class="btn--small recipe__btn"
+          href="${this._data.sourceUrl}"
+          target="_blank"
+        >
+          <span>Directions</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </a>
+      </div>
+    `;
+  }
+
+  _generateMarkupIngredient(ing) {
+    return `
+    <li class="recipe__ingredient">
+      <svg class="recipe__icon">
+        <use href="${icons}#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">${
+        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      }</div>
+      <div class="recipe__description">
+        <span class="recipe__unit">${ing.unit}</span>
+        ${ing.description}
+      </div>
+    </li>
+  `;
+  }
+}
+
+export default new asteroidView();
