e8e233452587e80036729bc2231fdee6db48b656
Reid McCulloch <rmcc0724@gmail.com>
2021-07-24 18:21:12 -0700

    Demo loaded

--- client/src/js/config.js
+++
@@ -1,2 +0,0 @@
-export const proxy = 'https://cors-anywhere.herokuapp.com/';
-export const key = 'xtcQn1fI4aTFJGdXDuVKxHMrUOEQIQbN6lYtSf4K';
---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fd9998db-835f-4c53-9ecb-e19d01e1fb2e';
+export const MODAL_CLOSE_SEC = 2.5;
